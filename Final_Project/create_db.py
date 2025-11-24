77e55b058f3076b044ae12bb1946ca0d9d406eaf
yflee <lee39.d.p@gmail.com>
2025-11-24 13:26:13 -0500

    Add Foreign Key and create normalized title_akas

--- Final_Project/create_db.py
+++ Final_Project/create_db.py
@@ -16,6 +16,7 @@ start_Statements = [
 
 # Normalized "title_basic"
 normalized_title_basic = [
+
     # Lookup: title_type
     # Split title type by comma, distinct types rows.
     """
@@ -24,6 +25,24 @@ normalized_title_basic = [
         title_type_name VARCHAR(64) NOT NULL
     );
     """,
+
+    # Core table: title_basics
+    # Foreign Key: title_type_id
+        """
+        CREATE TABLE IF NOT EXISTS title_basics (
+            tconst VARCHAR(12) NOT NULL PRIMARY KEY,
+            primaryTitle VARCHAR(512) NULL,
+            originalTitle VARCHAR(512) NULL,
+            isAdult TINYINT(1) NULL,
+            startYear SMALLINT NULL,
+            endYear SMALLINT NULL,
+            runtimeMinutes SMALLINT NULL,
+            title_type_id INT NULL,
+            CONSTRAINT fk_title_type_id
+                FOREIGN KEY (title_type_id) REFERENCES title_type(title_type_id)
+            ON UPDATE CASCADE ON DELETE CASCADE
+        );
+        """,
 
     # Lookup: genre
     # Split genres by comma, distinct genre rows.
@@ -34,39 +53,31 @@ normalized_title_basic = [
     );
     """,
 
-    # Core table: title_basics (normalized)
-    """
-    CREATE TABLE IF NOT EXISTS title_basics (
-        tconst VARCHAR(12) NOT NULL PRIMARY KEY,
-        primaryTitle VARCHAR(512) NULL,
-        originalTitle VARCHAR(512) NULL,
-        isAdult TINYINT(1) NULL,
-        startYear SMALLINT NULL,
-        endYear SMALLINT NULL,
-        runtimeMinutes SMALLINT NULL,
-        title_type_id INT NULL,
-    );
-    """,
-
     # Bridge table: title_genre (many-to-many)
+    # Foreign keys: tconst, genre_id
     """
     CREATE TABLE IF NOT EXISTS title_genre (
         id INT AUTO_INCREMENT PRIMARY KEY,
         tconst VARCHAR(12) NOT NULL,
         genre_id INT NOT NULL,
+        CONSTRAINT fk_tconst
+            FOREIGN KEY (tconst) REFERENCES title_basics(tconst)
+            ON UPDATE CASCADE ON DELETE CASCADE,
+        CONSTRAINT fk_genere_id
+            FOREIGN KEY (genre_id) REFERENCES genre(genre_id)
+            ON UPDATE CASCADE ON DELETE CASCADE
     );
     """
 ]
 
 normalized_name_basic = [
-
-    # Core table: name_basics (normalized, no multivalue fields)
+    # Core table: name_basics
     """
     CREATE TABLE IF NOT EXISTS name_basics (
         nconst VARCHAR(12) NOT NULL PRIMARY KEY,
         primaryName VARCHAR(256) NULL,
         birthYear SMALLINT NULL,
-        deathYear SMALLINT NULL
+        deathYear SMALLINT NULL,
     );
     """,
 
@@ -90,15 +101,88 @@ normalized_name_basic = [
 
     # Bridge: name_known_for (name_basics : title_basics with ordering)
     # Split knownForTitles
+    # Foreign Keys: nconst, profession_id
     """
     CREATE TABLE IF NOT EXISTS name_known_for (
         id INT AUTO_INCREMENT PRIMARY KEY,
         nconst VARCHAR(12) NOT NULL,
         tconst VARCHAR(12) NOT NULL,
         position SMALLINT NOT NULL,
+        CONSTRAINT fk_nconst
+            FOREIGN KEY (nconst) REFERENCES name_basics(nconst)
+            ON UPDATE CASCADE ON DELETE CASCADE,
+        CONSTRAINT fk_profession_id
+            FOREIGN KEY (profession_id) REFERENCES profession(profession_id)
+            ON UPDATE CASCADE ON DELETE CASCADE
     );
     """
 ]
+
+normalized_title_akas = [
+    # Core table: title_akas
+    # Foreign Key: titleId
+    """
+    CREATE TABLE IF NOT EXISTS title_akas (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        titleId VARCHAR(12) NOT NULL,
+        ordering INT NOT NULL,
+        title VARCHAR(512) NULL,
+        region_code VARCHAR(16) NULL,
+        language_code VARCHAR(16) NULL,
+        isOriginalTitle TINYINT(1) NULL,
+        CONSTRAINT fk_titleId
+            FOREIGN KEY (titleId) REFERENCES title_basics(tconst) 
+        ON UPDATE CASCADE ON DELETE CASCADE
+    );
+    """,
+
+    # Lookup: title_type (e.g., "tv", "festival", "dvd", etc.)
+    """
+    CREATE TABLE IF NOT EXISTS title_types (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        type_name VARCHAR(512) NOT NULL,
+    );
+    """,
+
+    # Bridge: title_aka_type
+    """
+    CREATE TABLE IF NOT EXISTS title_aka_type (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        title_akas_id INT NOT NULL,
+        title_types_id INT NOT NULL,
+        CONSTRAINT fk_title_akas_id
+            FOREIGN KEY (title_akas_id) REFERENCES title_akas(id) 
+        ON UPDATE CASCADE ON DELETE CASCADE,
+        CONSTRAINT fk_title_types_id
+            FOREIGN KEY (title_types_id) REFERENCES title_types(id) 
+        ON UPDATE CASCADE ON DELETE CASCADE
+    );
+    """,
+
+    # Lookup: title_attribute (e.g., "working title", "short title", etc.)
+    """
+    CREATE TABLE IF NOT EXISTS title_attribute (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        attribute_name VARCHAR(128) NOT NULL,
+    );
+    """,
+    
+    # Bridge: title_aka_attribute
+    """
+    CREATE TABLE IF NOT EXISTS title_aka_attribute (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        title_akas_id INT NOT NULL,
+        title_attribute_id INT NOT NULL,
+        CONSTRAINT fk_title_akas_id
+            FOREIGN KEY (title_akas_id) REFERENCES title_akas(id) 
+        ON UPDATE CASCADE ON DELETE CASCADE,
+        CONSTRAINT fk_title_types_id
+            FOREIGN KEY (title_attribute_id) REFERENCES title_attribute(id) 
+        ON UPDATE CASCADE ON DELETE CASCADE
+    );
+    """,
+]
+
 
 DDL_STATEMENTS = [
     normalized_title_basic,
